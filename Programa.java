53b262a27156d651fdd34ea138b7c6967589f78a
Maslow5941 <maslow5941@hotmail.com>
2022-09-14 12:00:02 -0300

    adicionando exercicio

---
+++ Programa.java
@@ -0,0 +1,29 @@
+package aplicacao;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+
+}
